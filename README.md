094c8998657b27a46572ed28608c2e7ad23eaaea
manlikeNacho <eiheanacho52@gmail.com>
2022-05-31 22:36:37 +0100

    the first repo

---
+++ README.md
@@ -0,0 +1,1 @@
+A simple app for uploading pictures ,hosted on firebase.
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Title from './comps/Title';
+import UploadForm from './comps/UploadForm.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Title/>
+      <UploadForm />
+    </div>
+  );
+}
+
+export default App;
